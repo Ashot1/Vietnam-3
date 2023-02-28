a2a7ec49b3d65fe498bfe9e7ac04590cd1b15f9f
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-02-28 17:49:39 +0300

    3.2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {useState} from "react";
+import {Route, Routes, useLocation} from "react-router-dom";
 import './App.css';
 
 import StartPage from './pages/start/start'
@@ -16,63 +16,77 @@ import Header from './components/header/header'
 import Slider from './components/slider/slider'
 import Startmenu from './components/startmenu/startmenu'
 import Menu from './components/menu/menu'
-import Footer from "./components/footer/footer";
+import {CSSTransition, TransitionGroup} from "react-transition-group";
+import styles from "./components/menu/menu.module.css";
 
 export default function App() {
-
-    const [MenuOpen, setMenuOpen] = useState(false)
-    const [Routers, setRouters] = useState([
-        {
-            content: <StartPage><Slider /><Startmenu /></StartPage>,
-            url: '/'
-        },
-        {
-            content: <CalculatorPage />,
-            url: '/Calculator'
-        },
-        {
-            content: <ConsolePage />,
-            url: '/Console'
-        },
-        {
-            content: <DownloadPage />,
-            url: '/Download'
-        },
-        {
-            content: <SRMPage />,
-            url: '/SocialRatingMiner'
-        },
-        {
-            content: <SettingsPage />,
-            url: '/Settings'
-        },
-        {
-            content: <TodoPage />,
-            url: '/Todo'
-        },
-        {
-            content: <NotFoundedPage />,
-            url: '*'
-        },
-    ])
-
-    function MenuActivation(value) {
-        return setMenuOpen((MenuOpen) => !MenuOpen);
-    }
-
-    return (
-        <div className="app">
-            <BrowserRouter>
-                <Header BurgerChange={MenuActivation} />
-                <Menu MenuChanges={MenuOpen} />
-                <Routes>
-                    {Routers.map((item) => {
-                        return <Route path={item.url} element={item.content} />
-                    })}
-                </Routes>
-                <Footer />
-            </BrowserRouter>
-        </div>
-    );
+	
+	const [MenuOpen, setMenuOpen] = useState(false)
+	const [Routers, setRouters] = useState([
+		{
+			content: <StartPage><Slider/><Startmenu/></StartPage>,
+			url: '/'
+		},
+		{
+			content: <CalculatorPage/>,
+			url: '/Calculator'
+		},
+		{
+			content: <ConsolePage/>,
+			url: '/Console'
+		},
+		{
+			content: <DownloadPage/>,
+			url: '/Download'
+		},
+		{
+			content: <SRMPage/>,
+			url: '/SocialRatingMiner'
+		},
+		{
+			content: <SettingsPage/>,
+			url: '/Settings'
+		},
+		{
+			content: <TodoPage/>,
+			url: '/Todo'
+		},
+		{
+			content: <NotFoundedPage/>,
+			url: '*'
+		},
+	])
+	
+	function MenuActivation(value) {
+		return setMenuOpen((MenuOpen) => !MenuOpen);
+	}
+	
+	const location = useLocation()
+	return (
+		<div className="app">
+			<Header BurgerChange={MenuActivation}/>
+			<Menu MenuChanges={MenuOpen}/>
+			<TransitionGroup>
+				<CSSTransition
+					key={location.pathname}
+					timeout={400}
+					classNames={{
+						enterActive: `block_visible`,
+						exitActive: `block_close`,
+					}}
+				>
+					<div className={styles.huy}>
+						<Routes>
+							{Routers.map((item) => {
+								return (
+									<Route key={item.url} path={item.url} element={item.content}/>
+								)
+							})}
+						</Routes>
+					</div>
+				</CSSTransition>
+			</TransitionGroup>
+		</div>
+	);
 }
 
--- src/pages/download/download.js
+++ src/pages/download/download.js
@@ -1,8 +1,42 @@
+import styles from "./download.module.css";
+import {useState} from "react";
+
+import Vietnam from './image/logo.png'
+import MCK from './image/logo MCK.png'
+import MINISTALL from './image/utorrent.png'
+import ESL from './image/windows.png'
+
+import VietnamDownload from './files/app-release.apk'
+import MCKDownload from './files/MCK.apk'
+import MINSTALLDownload from './files/MInstAll_v19.01.2020.torrent'
+import ESLDownload from './files/EmptyStandbyList.exe'
+
 
 export default function DownloadPage() {
-    return (
-        <div>
-
-        </div>
-    )
+	const [Downloads, setDownloads] = useState([
+		{url: VietnamDownload, name: 'Vietnam app', type: 'apk', img: Vietnam},
+		{url: MCKDownload, name: 'MCK app', type: 'apk', img: MCK},
+		{url: MINSTALLDownload, name: 'MininstAll', type: 'torrent', img: MINISTALL},
+		{url: ESLDownload, name: 'EmptyStandByList', type: 'exe', img: ESL},
+	])
+	
+	return (
+		<main className={styles.main}>
+			<div className={styles.content}>
+				<ul className={styles.DownloadList}>
+					{Downloads.map(item => {
+						return (
+							<li key={item.name} className={styles.Download}>
+								<a href={item.url}>
+									<img src={item.img}/>
+									<p>{item.name}</p>
+									<span>{item.type}</span>
+								</a>
+							</li>
+						)
+					})}
+				</ul>
+			</div>
+		</main>
+	)
 }
