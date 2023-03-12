116cb3c98e50a012bd2edd64f9d69ca0d428d245
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-03-13 02:52:45 +0300

    3.3

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,20 +1,25 @@
 import styles from './header.module.css'
-import { useState } from 'react'
-import { Link } from 'react-router-dom';
+import {useState} from 'react'
+import {Link} from 'react-router-dom';
 
 export default function Header(props) {
-    const [BurgerState, setBurgerState] = useState(false)
-
-    return (
-        <header className={styles.header}>
-            <a className={styles.menuburgerWrapper} onClick={() => { setBurgerState(!BurgerState); props.BurgerChange(BurgerState) }}>
-                <button className={BurgerState ? styles.menuburgerActive : styles.menuburger}></button>
-            </a>
-            <div className={styles.logoNamePosition}>
-                <Link to={`/`} className={styles.logoName}>
-                    Vietnam
-                </Link>
-            </div>
-        </header>
-    )
+	const [BurgerState, setBurgerState] = useState(false)
+	
+	return (
+		<header className={styles.headerWrapper}>
+			<div className={styles.header}>
+				<a className={styles.menuburgerWrapper} onClick={() => {
+					setBurgerState(!BurgerState);
+					props.BurgerChange(BurgerState)
+				}}>
+					<button className={BurgerState ? styles.menuburgerActive : styles.menuburger}></button>
+				</a>
+				<div className={styles.logoNamePosition}>
+					<Link to={`/`} className={styles.logoName}>
+						Vietnam
+					</Link>
+				</div>
+			</div>
+		</header>
+	)
 }
