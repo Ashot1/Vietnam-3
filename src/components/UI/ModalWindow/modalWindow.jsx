a2976c5298ba38c5c50b08cf2c6718a0044f6b69
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-04-20 19:12:00 +0300

    2.9.8.1

--- src/components/UI/ModalWindow/modalWindow.jsx
+++ src/components/UI/ModalWindow/modalWindow.jsx
@@ -12,7 +12,7 @@ export default function ModalWindow({CloseSetting, children, classModal}) {
 				onClick={(e) => e.stopPropagation()}
 				initial={{scale: 0.5}}
 				animate={{scale: 1}}
-				exit={{scale: 0.5}}
+				exit={{scale: 0.5, opacity: 0}}
 			>
 				<div className={styles.body}>
 					{children}
