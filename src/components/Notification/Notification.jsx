6f4fdc56f23558b1bc31617d76e84a68ccf951f4
TvoyOtec <44757072+TvoyOtec@users.noreply.github.com>
2023-06-01 20:04:21 +0300

    3.0.3 hotfix

--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -4,8 +4,7 @@ export default function Notification({url, text}) {
 	return (
 		<div className={styles.wrapper}>
 			<div className={styles.content}>
-				<p>Версия этого приложения устарела.</p>
-				<a href={url} target="_blank"> {text}</a>
+				<p>Версия этого приложения устарела. <a href={url} target="_blank"> {text}</a></p>
 			</div>
 		</div>
 	);
